e0e8721c874a41ec8bf04416b285cac6894e80b9
Midory Cas <midoryjcp3103@gmail.com>
2024-08-28 19:33:53 +0000

    challenge 1

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -3,7 +3,8 @@ SELECT
     date_date, 
     orders_id,
     revenue,
-    CAST(p.purchase_price AS FLOAT64),
+    quantity,
+    CAST(p.purchase_price AS FLOAT64) AS purchase_price,
     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
     s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
 FROM {{ref('stg_raw__sales')}} s
